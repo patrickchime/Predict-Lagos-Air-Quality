6189c5e9ff69ae1e46bd413bea98886f15a5a39c
“Patrick <patrick.chime234@gmail.com>
2024-05-17 11:09:03 +0100

    new commit

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import sklearn
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-from sklearn.metrics import mean_absolute_error
+
 
 import data # Modules
 
